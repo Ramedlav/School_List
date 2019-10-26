1ec0fa7b26e72f49092255bb0a4ea54e3e870daa
Ramedlav <ochev.vladimir@gmail.com>
2019-10-26 17:46:51 +0200

    comm1

--- php/addUser.php
+++ php/addUser.php
@@ -5,15 +5,16 @@ $one = new user();
  echo('status='.$_POST['status']);
 switch ($_POST['status']) {
     case '1':
-    		if(!isset($_POST['status'])){$_POST['status']=' ';}
-        	if(!isset($_POST['name'])){$_POST['name']=' ';}
-        	if(!isset($_POST['surname'])){$_POST['surname']=' ';}
-        	if(!isset($_POST['threename'])){$_POST['threename']=' ';}
-        	if(!isset($_POST['faculty'])){$_POST['faculty']=' ';}
-        	if(!isset($_POST['speciality'])){$_POST['speciality']=' ';}
-        	if(!isset($_POST['group'])){$_POST['group']=' ';}
-        	if(!isset($_POST['gender'])){$_POST['gender']=' ';}
-        	if(!isset($_POST['burn'])){$_POST['burn']='1111-11-11';}
+        // check commit
+//    		if(!isset($_POST['status'])){$_POST['status']=' ';}
+//        	if(!isset($_POST['name'])){$_POST['name']=' ';}
+//        	if(!isset($_POST['surname'])){$_POST['surname']=' ';}
+//        	if(!isset($_POST['threename'])){$_POST['threename']=' ';}
+//        	if(!isset($_POST['faculty'])){$_POST['faculty']=' ';}
+//        	if(!isset($_POST['speciality'])){$_POST['speciality']=' ';}
+//        	if(!isset($_POST['group'])){$_POST['group']=' ';}
+//        	if(!isset($_POST['gender'])){$_POST['gender']=' ';}
+//        	if(!isset($_POST['burn'])){$_POST['burn']='1111-11-11';}
         $one->AddUser(
         	$_POST['status'],
         	$_POST['name'],
