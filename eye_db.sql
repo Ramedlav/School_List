9f771e392a404cb6680531c2d393dce88ef0ca51
Владимир Алексеевич Очев <51778553+Ramedlav@users.noreply.github.com>
2019-10-27 02:09:04 +0100

    add database
    add database

---
+++ eye_db.sql
@@ -0,0 +1,383 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 27 2019 г., 04:06
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `eye_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `course` varchar(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `deportaments`
+--
+
+CREATE TABLE `deportaments` (
+  `id` int(11) NOT NULL,
+  `deportament_name` varchar(28) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `deportaments`
+--
+
+INSERT INTO `deportaments` (`id`, `deportament_name`) VALUES
+(0, 'programming ingenery'),
+(1, 'apply math');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `faculty_name` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `faculty_name`) VALUES
+(1, 'AppliedMath'),
+(2, 'economics and audit'),
+(3, 'economic cybirnetics');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `group_name` varchar(5) NOT NULL,
+  `id_faculty` int(11) NOT NULL,
+  `id_speciality` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rating`
+--
+
+CREATE TABLE `rating` (
+  `id` int(20) NOT NULL,
+  `rating` varchar(8) NOT NULL,
+  `id_student` int(20) DEFAULT NULL,
+  `id_subject` int(5) DEFAULT NULL,
+  `id_teacher` int(11) DEFAULT NULL,
+  `id_status_rating` int(5) DEFAULT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `rating`
+--
+
+INSERT INTO `rating` (`id`, `rating`, `id_student`, `id_subject`, `id_teacher`, `id_status_rating`, `date`) VALUES
+(1, '75', 1, 1, 1, 1, '2019-09-25'),
+(2, '65', 2, 1, 2, 1, '2019-09-25'),
+(3, '60', 2, 1, 2, 1, '2019-10-27');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sex`
+--
+
+CREATE TABLE `sex` (
+  `id` int(11) NOT NULL,
+  `gender` varchar(6) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `sex`
+--
+
+INSERT INTO `sex` (`id`, `gender`) VALUES
+(1, 'male'),
+(2, 'famale');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `speciality`
+--
+
+CREATE TABLE `speciality` (
+  `id` int(11) NOT NULL,
+  `speciality_name` varchar(35) NOT NULL,
+  `id_faculty` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `speciality`
+--
+
+INSERT INTO `speciality` (`id`, `speciality_name`, `id_faculty`) VALUES
+(1, 'mathematic', 1),
+(2, 'programming ingenering', 1),
+(3, 'informathic', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `status_name` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `status`
+--
+
+INSERT INTO `status` (`id`, `status_name`) VALUES
+(1, 'student'),
+(2, 'teacher');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(35) NOT NULL,
+  `surname` varchar(35) NOT NULL,
+  `id_status` int(11) DEFAULT NULL,
+  `login` varchar(11) DEFAULT NULL,
+  `password` varchar(12) DEFAULT NULL,
+  `father_name` varchar(35) DEFAULT NULL,
+  `burn_date` date DEFAULT NULL,
+  `id_sex` int(11) DEFAULT NULL,
+  `id_faculty` int(11) DEFAULT NULL,
+  `id_speciality` int(11) DEFAULT NULL,
+  `id_group` int(11) DEFAULT NULL,
+  `id_course` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `surname`, `id_status`, `login`, `password`, `father_name`, `burn_date`, `id_sex`, `id_faculty`, `id_speciality`, `id_group`, `id_course`) VALUES
+(1, 'wqw', 'wqww', 1, NULL, NULL, 'qwqw', '2000-10-10', 1, 1, 1, 1, NULL),
+(2, 'sas', 'sass', 1, NULL, NULL, 'sassas', '2000-10-10', 1, 1, 1, 1, NULL),
+(3, 'dodo', 'dodo', 1, NULL, NULL, 'dodo', '2000-10-10', 1, 1, 1, 1, NULL),
+(4, 'wwwe', 'weww', 2, NULL, NULL, ' ', NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'ddd', 'ddd', 2, NULL, NULL, ' ', NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'tet', 'tet', 1, NULL, NULL, 'tet', '2000-10-10', 1, 1, 1, 1, NULL),
+(7, 'gangl', 'big', 1, NULL, NULL, 'biger', '1989-12-22', 1, 1, 1, 1, NULL),
+(8, 'greg', 'wilson', 1, NULL, NULL, 'stain', '2000-10-10', 2, 1, 1, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `subject_name` varchar(24) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_name`) VALUES
+(0, 'mathematical analysis'),
+(1, 'mathematical logics');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `teacher_name` varchar(26) NOT NULL,
+  `teacher_surname` varchar(25) NOT NULL,
+  `teacher_three_name` varchar(25) NOT NULL,
+  `deportament` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `teacher_name`, `teacher_surname`, `teacher_three_name`, `deportament`) VALUES
+(1, 'andrey', 'lamzuk', 'alexeevich', '1'),
+(2, 'svetlana', 'kazakova', 'anatolievna', '1'),
+(3, 'andrey', 'ganja', 'pavlovich', '0'),
+(6, 'georgiy', 'perelman', 'jakovlevich', '1');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `deportaments`
+--
+ALTER TABLE `deportaments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `sex`
+--
+ALTER TABLE `sex`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `speciality`
+--
+ALTER TABLE `speciality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `sex`
+--
+ALTER TABLE `sex`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `speciality`
+--
+ALTER TABLE `speciality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
